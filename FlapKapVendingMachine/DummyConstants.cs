c01b8e162f6b34c6642f029962c6ffbf0c2f3fd9
Hesham Elbahey <hesham.elbahi.std@cis.asu.edu.eg>
2022-01-08 14:46:46 +0200

    Initial commit

---
+++ FlapKapVendingMachine/DummyConstants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace FlapKapVendingMachine
+{
+    public class DummyConstants
+    {
+        public const string ConnectionString = @"Server=localhost,1433;
+        Database=VendingMachine;
+        User Id=machineUser;
+        Password=123456;
+        Integrated Security=False;
+        MultipleActiveResultSets=True";
+
+        public const string JWTSecret = "Secret";
+        public const string JWTIssuer = "Issuer";
+        public const string JWTAudience = "Audience";
+        public const int JWTAccessTokenExpiration = 5;
+        public const int JWTRefreshTokenExpiration = 30;
+    }
+}
