c368bd336ee46c295ac68376b061270ea81a47f4
Hesham Elbahey <hesham.elbahi.std@cis.asu.edu.eg>
2022-01-08 20:58:55 +0200

    Add unique column checks

---
+++ FlapKapVendingMachine/Extensions/DbSetProductExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using FlapKapVendingMachine.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlapKapVendingMachine.Extensions
+{
+    public static  class DbSetProductExtensions
+    {
+        public static async Task<bool> CheckUniqueProductNameAsync(this DbSet<Product> products, string productName)
+        {
+            return !(await products.AnyAsync(p => p.ProductName == productName));
+        }
+    }
+}
---
+++ FlapKapVendingMachine/Extensions/DbSetUserExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using FlapKapVendingMachine.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlapKapVendingMachine.Extensions
+{
+    public static class DbSetUserExtensions
+    {
+        public static async Task<bool> CheckUniqueUserNameAsync(this DbSet<ApplicationUser> users, string userName)
+        {
+            return !(await users.AnyAsync(u => u.UserName == userName));
+        }
+    }
+}
